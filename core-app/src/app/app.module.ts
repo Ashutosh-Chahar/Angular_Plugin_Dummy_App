112c6cd4747d8e580ab6acbac9e65ba4501aa5e5
Ashutosh Chahar <ashutoshchahar264@gmail.com>
2019-12-21 03:20:32 +0530

    "++"

--- core-app/src/app/app.module.ts
+++ core-app/src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { COMPILER_OPTIONS,CompilerFactory,Compiler,NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// Required by the SystemJS
+import { JitCompilerFactory } from '@angular/platform-browser-dynamic';
+
+export function createCompiler(fn: CompilerFactory): Compiler {     
+  return fn.createCompiler();
+}
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
-  providers: [],
+  providers: [{
+    provide: COMPILER_OPTIONS,
+    useValue: {},
+    multi: true
+  },
+  {
+    provide: CompilerFactory,
+    useClass: JitCompilerFactory,
+    deps: [COMPILER_OPTIONS]
+  },
+  {
+    provide: Compiler,
+    useFactory: createCompiler,
+    deps: [CompilerFactory]
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
